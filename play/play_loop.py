2fa5548bdcd148abf392cf670b948fb15fcf8bce
Wei Min Loh <lohwm98@hotmail.com>
2020-11-05 14:07:28 -0500

    docs[play_loop.py]: Added documentation to all functions

--- play/play_loop.py
+++ play/play_loop.py
@@ -6,18 +6,33 @@ from inference.infer_action import get_action
 
 
 class PlayLoop:
+    __doc__ = '''
+    An interactive REPL environment for play with a trained chess AI
+    '''
     TRACE_FORMAT = '{:<12}{:<20}{:<12}{}'
     TRACE_HEADER = ('WHITE', 'move', 'BLACK', 'move')
 
     def __init__(self, policy):
+        '''
+        Constructs a PlayLoop instance
+
+        :param policy: PolicyModel - primary AI agent to simulate
+        '''
         self.policy = policy
         self.player_white = None
         self.board = None
         self.keep_trace = False
         self.trace = None
 
-    def init_game(self, player_side, keep_trace=False, force_new=False):
-        if not force_new and self.board is not None:
+    def init_game(self, player_side, keep_trace=False):
+        '''
+        Sets up a game and indicating the side to play as by the player
+
+        :param player_side: 'w'/'b' - side to play as
+        :param keep_trace: bool - if True, accumulates the trace for the entire game
+        :return: None
+        '''
+        if self.board is not None:
             raise RuntimeWarning('Board already initiatized, set force_new=True to force override')
 
         if player_side == 'w':
@@ -32,7 +47,21 @@ class PlayLoop:
             self.keep_trace = keep_trace
             self.trace = list()
 
+    def reset(self):
+        '''
+        Resets the PlayLoop state (except the trace)
+
+        :return: None
+        '''
+        self.board = None
+        self.keep_trace = False
+
     def loop(self):
+        '''
+        Runs the loop until the termination of a game
+
+        :return: None
+        '''
         if self.board is None:
             raise RuntimeError('init_game was not called to configure game settings!')
 
@@ -46,11 +75,9 @@ class PlayLoop:
                 self._store_trace(act_uci, trace_collector)
 
         while not self.board.is_game_over():
-            # print(tensor_decode(tensor_encode(self.board, flip=not self.player_white)))
             print(self.board)
 
             move = self._get_player_move()
-
             if self.keep_trace:
                 self._store_trace(move, trace_collector)
 
@@ -64,15 +91,31 @@ class PlayLoop:
         print('Game completed')
 
     def get_trace(self, printable=True):
+        '''
+        Returns the trace
+
+        :param printable: bool - If True, returns a printable and formamted string of the trace
+        :return: str
+        '''
         if printable:
             return '\n'.join(self.trace)
         return self.trace
 
     def _get_action_from_policy(self):
+        '''
+        Gets UCI representation of the move using the policy loaded
+
+        :return: str
+        '''
         src, tgt, _ = self.policy.infer(tensor_encode(self.board, flip=self.player_white))
         return get_action(self.board, src, tgt)
 
     def _get_player_move(self):
+        '''
+        Obtains the move from the player by command line and pushes the move on the board
+
+        :return: str
+        '''
         while True:  # handles invalid player moves
             try:  # TODO: Use proper validation checks if possible (non-urgent)
                 move = input('Enter your move: ')
@@ -84,6 +127,14 @@ class PlayLoop:
         return move
 
     def _store_trace(self, move, trace_collector):
+        '''
+        Collects the trace onto trace_collector and once white and black has made the move,
+            append to the main trace list
+
+        :param move: str - UCI representation of the move
+        :param trace_collector: list(str) - string accumulator
+        :return: None
+        '''
         trace_collector.append(str(self.board))
         trace_collector.append(move)
         if len(trace_collector) == 4:
